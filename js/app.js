f71a8fc4f675d803768ec4bfed12059dd33ef40d
Olivia <gassolivia@gmail.com>
2021-09-08 17:28:47 +0200

    Edit read-more

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function showMore(){
+    //removes the link
+    document.getElementById('link').parentElement.removeChild('link');
+    //shows the #more
+    document.getElementById('more').style.display = "block";
+}
